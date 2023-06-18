1e4e606058ec479304d86f96a9ad8aa6734ce38f
Dante Trisciuzzi <dante.trisciuzzi@snhu.edu>
2023-06-17 20:45:33 -0400

    fix parse on final byte

--- main.zig
+++ main.zig
@@ -3,28 +3,42 @@ const out = std.debug.print;
 
 pub const byte_pattern = struct { byte_value: u8, wildcard: bool };
 
-pub const pattern_array = std.BoundedArray(byte_pattern, 256);
+const pattern_array_size: u8 = 128;
+pub const pattern_array = std.BoundedArray(byte_pattern, pattern_array_size);
 
-fn byteSplit(str: []const u8, delim: u8, array: *pattern_array) !void {
+pub fn parsePattern(str: []const u8, delim: u8, array: *pattern_array) !void {
     var last_delim_loc: usize = 0;
     var num_bytes: usize = 0;
 
     for (0..str.len) |i| {
-        const last_byte = i == str.len - 1;
-        const c_is_delim = str[i] == delim;
+        const is_last_byte = (i == str.len - 1);
+        const char_is_delim = (str[i] == delim);
 
-        if (c_is_delim != last_byte) {
-            var slc = if (last_byte) str[last_delim_loc..] else str[last_delim_loc..i];
-            if (std.mem.eql(u8, slc, "??")) {
+        if (char_is_delim or is_last_byte) {
+
+            // Slice to the end or current index based on index position
+            var byte_slice = if (is_last_byte) str[i - 1 ..] else str[i - 2 .. i];
+
+            if (byte_slice.len < 2) {
+                break;
+            }
+            // Add wildcard if wildcard byte found
+            if (std.mem.eql(u8, byte_slice, "??")) {
                 _ = try array.append(byte_pattern{ .byte_value = 0x00, .wildcard = true });
                 num_bytes += 1;
-            } else {
-                const parsed = try std.fmt.parseUnsigned(u8, slc, 16);
-                if (@TypeOf(parsed) == u8) {
-                    _ = try array.append(byte_pattern{ .byte_value = parsed, .wildcard = false });
-                    num_bytes += 1;
-                }
+                continue;
             }
+
+            // Parse our value from the byte string
+            // Note radix is 16 since the bytes are in hex
+            const parsed = try std.fmt.parseUnsigned(u8, byte_slice, 16);
+
+            if (@TypeOf(parsed) == u8) {
+                _ = try array.append(byte_pattern{ .byte_value = parsed, .wildcard = false });
+                num_bytes += 1;
+            }
+
+            // Update delim marker
             last_delim_loc = i + 1;
         }
     }
@@ -36,7 +50,7 @@ fn byteSplit(str: []const u8, delim: u8, array: *pattern_array) !void {
 
 //     var arr = pattern_array{};
 
-//     _ = try byteSplit(pattern_1, ' ', &arr);
+//     _ = try parsePattern(pattern_1, ' ', &arr);
 //     out("\n", .{});
 //     var slc = arr.constSlice();
 //     for (0..arr.len) |i| {
